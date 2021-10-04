4a2b71b9bd68e3a82c023722484137f3eda97972
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-10-04 18:38:55 -0300

    requisito 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-orderofunctions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-orderofunctions/script.js
@@ -1,14 +1,29 @@
-const newEmployees = () => {
-  const employees = {
-    id1: createmployees('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: createmployees('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: createmployees('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
-};
+// const newEmployees = () => {
+  // const employees = {
+    // id1: createmployees('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    // id2: createmployees('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    // id3: createmployees('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  // }
+  // return employees;
+// };
 
-const createmployees = (name) => {
-  const resultado ={nome: name, email: `${name}@trybe.com`};
+// const createmployees = (name) => {
+  // const resultado ={nome: name, email: `${name}@trybe.com`};
+  // return resultado;
+// }
+
+const resultadoSorteio = (aposta, checarAposta) =>{
+  numeroSorteio = Math.ceil(Math.random()*5) ;
+  const resultado = checarAposta(numeroSorteio, aposta);
   return resultado;
 }
-console.log(newEmployees());
+const checarAposta = (numeroSorteio, aposta) => {
+  let resultado;
+  if (aposta === numeroSorteio) {
+    resultado = 'Parabéns você ganhou';
+  } else {
+    resultado = 'Tente novamente';
+  }
+  return resultado;
+}
+console.log(resultadoSorteio(1, checarAposta));
